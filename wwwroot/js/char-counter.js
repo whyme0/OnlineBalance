5c9cc02355e3bfcef9edb83fed3ba245851b1fec
whyme0 <sparkjetstudiost@gmail.com>
2023-05-17 22:34:06 +0300

    add chars counter to description field

---
+++ wwwroot/js/char-counter.js
@@ -0,0 +1,19 @@
+﻿let inputFieldId = document.currentScript.getAttribute("inputElementId");
+let outputFieldId = document.currentScript.getAttribute("outputElementId");
+let maxlen = parseInt(document.currentScript.getAttribute("maxlength"), 10);
+
+let inputField = document.getElementById(inputFieldId);
+let outputField = document.getElementById(outputFieldId);
+
+if (inputField == null) {
+    console.error("Input field not found! Check 'inputElementId' attribute!")
+}
+if (outputField == null) {
+    console.error("Input field not found! Check 'outputElementId' attribute!")
+}
+
+inputField.addEventListener("keyup", (event) => {
+    let len = inputField.value.length;
+    let message = `You're entered ${len} of ${maxlen} chars`;
+    outputField.innerText = message;
+});
