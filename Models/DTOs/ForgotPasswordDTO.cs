f7c5edaa74ec98bbae5f092992009dc40213d5ac
whyme0 <sparkjetstudiost@gmail.com>
2023-08-07 22:34:19 +0300

    created

---
+++ Models/DTOs/ForgotPasswordDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineBalance.Models
+{
+    public class ForgotPasswordDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ Models/DTOs/ResetPasswordDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineBalance.Models
+{
+    public class ResetPasswordDTO
+    {
+        [DataType(DataType.Password)]
+        [StringLength(128, MinimumLength = 8, ErrorMessage = "Too short or too long password")]
+        [Required(ErrorMessage = "Password field is required")]
+        [Display(Name = "New password")]
+        public string Password { get; set; }
+        [Display(Name = "Reapeat password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords didn't match")]
+        [StringLength(128, MinimumLength = 8, ErrorMessage = "Too short or too long password")]
+        [Required(ErrorMessage = "Repeat your password")]
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
