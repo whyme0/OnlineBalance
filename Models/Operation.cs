5237d09056829f207cbfbafc86b6661670d16c7f
whyme0 <sparkjetstudiost@gmail.com>
2023-08-19 15:52:08 +0300

    task №5 completed

--- Models/Operation.cs
+++ Models/Operation.cs
@@ -13,10 +13,10 @@
     public class TemporaryOperation
     {
         public Guid Id { get; set; }
-        public DateTime? Date { get; set; }
         public double Amount { get; set; }
         public string? Description { get; set; }
         public long SenderNumber { get; set; }
         public long RecipientNumber { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
